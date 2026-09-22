429e04137ec5e3240d1f760f02ddf81c38d52128
Nithyaccs <147238485+Nithyaccs@users.noreply.github.com>
2026-09-22 12:28:18 +0530

    Time: 2 ms (37.03%) | Memory: 42.7 MB (74.54%) - LeetSync

---
+++ 394-decode-string/decode-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> nums = new Stack<>();
+        Stack<String> strs = new Stack<>();
+
+        int num = 0;
+        String cur = "";
+
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+            } 
+            else if (c == '[') {
+                nums.push(num);
+                strs.push(cur);
+                num = 0;
+                cur = "";
+            } 
+            else if (c == ']') {
+                int k = nums.pop();
+                String prev = strs.pop();
+
+                cur = prev + cur.repeat(k);
+            } 
+            else {
+                cur += c;
+            }
+        }
+
+        return cur;
+    }
+}
